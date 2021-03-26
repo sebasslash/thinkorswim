9314e82f3e112482a60ecbd47ee11c11f2f177bd
Joseph Wolff <joe@osdf.com>
2021-03-26 10:04:51 -0700

    Tweak characters fed to installer, remove autorun to run the default bash in container, which works - JJW

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ docker run -ti \
        -e DISPLAY=$DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        linux-thinkorswim \
-       /usr/local/thinkorswim/thinkorswim
+
+#       /usr/local/thinkorswim/thinkorswim
